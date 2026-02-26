d7c1eaa79e0a4f474bd24c2a30e8b9dd69e95ca0
kalbhoranshula2007-hub <kalbhoranshula2007@gmail.com>
2026-02-26 20:33:41 +0530

    Create unit3 .shapes.py

---
+++ unit3 .shapes.py
@@ -0,0 +1,13 @@
+import math
+
+# Area of Circle
+def circle_area(radius):
+    return math.pi * radius * radius
+
+# Area of Rectangle
+def rectangle_area(length, width):
+    return length * width
+
+# Area of Triangle
+def triangle_area(base, height):
+    return 0.5 * base * height
